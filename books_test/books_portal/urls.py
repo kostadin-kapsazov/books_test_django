c47a5fa8849a2b937c6110d3e378509ab226ddd3
Kostadin Kapsazov <kostadin.kapsazov@sentido.bg>
2013-10-28 11:00:52 +0200

    Migrate to CBV fix

--- books_test/books_portal/urls.py
+++ books_test/books_portal/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import patterns, url
 from .views import BooksDetailView
 from .views import BooksCreateView
 from .views import BooksUpdateView
-from .views import BooksView
+from .views import BooksListView
 
 urlpatterns = patterns('',
-    url(r'^books/$', BooksView.as_view(), name="books_portal"),
+    url(r'^books/$', BooksListView.as_view(), name="books_portal"),
     url(r'^books/(?P<pk>\d+)/view/$', BooksDetailView.as_view(), name="books_details"),
     url(r'^books/add/$', BooksCreateView.as_view(), name="books_add"),
     url(r'^books/(?P<pk>\d+)/edit/$', BooksUpdateView.as_view(), name="books_edit"),
--- books_test/books_portal/views.py
+++ books_test/books_portal/views.py
@@ -1,73 +1,26 @@
 # coding: utf-8
+from django.views.generic import ListView, UpdateView, CreateView, DetailView
 
-
-from django.views.generic import TemplateView, ListView, UpdateView, CreateView, DetailView, View
-from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
-from django.http import Http404
-
-from .models import BooksForm
 from .models import Books
+
 
 class BooksDetailView(DetailView):
     model = Books
     template_name = "books_portal/details.html"
+
 
 class BooksCreateView(CreateView):
     model = Books
     template_name = "books_portal/add.html"
     success_url = 'books_portal'
 
+
 class BooksUpdateView(UpdateView):
     model = Books
     template_name = "books_portal/add.html"
     success_url = 'books_portal'
 
-class BooksView(View):
+
+class BooksListView(ListView):
     model = Books
     template_name = "books_portal/index.html"
-
-
-class BooksView(TemplateView):
-    template_name = "books_portal/index.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(BooksView, self).get_context_data(**kwargs)
-        books_list = list(Books.objects.all())
-        context.update({
-            'books_list': books_list,
-        })
-
-        return context
-
-
-class AddView(TemplateView):
-    template_name = "books_portal/add.html"
-
-    def post(self, request, *args, **kwargs):
-        f = BooksForm(request.POST)
-        f.save()
-
-        return HttpResponseRedirect(reverse('books_portal_books'))
-
-
-class DetailsView(TemplateView):
-    template_name = "books_portal/details.html"
-
-    def get_context_data(self, **kwargs):
-        context = super(DetailsView, self).get_context_data(**kwargs)
-
-        book_id = kwargs.get('book_id')
-
-        result = list(Books.objects.filter(id=book_id))
-
-        if result:
-            book = result[0]
-        else:
-            raise Http404()
-
-        context.update({
-            'book': book,
-        })
-
-        return context
